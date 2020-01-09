6d6e06b8050e8b9f36e05a54f6e9be1e4f1c2844
Jose Laruta <eduardo.laruta@gmail.com>
2020-01-09 00:20:14 -0400

    rotacion y color de fichas

--- include/tetris.h
+++ include/tetris.h
@@ -2,11 +2,68 @@
 #define TETRIS_H
 #include <stdint.h>
 #include <vector>
+#include <string>
+#include <bits/stdc++.h>
+
+enum Dir
+{
+    RIGHT,
+    LEFT,
+    N_DIRS
+};
+
 struct Tile 
 {
     int w;
     int h;
-    std::vector<uint8_t> shape;
-    Tile(std::vector<uint8_t> shape):w(4), h(4), shape(shape){};
+    std::vector<std::string> shape;
+    olc::Pixel color;
+    Tile():w(4), h(4), color(olc::RED){};
+    Tile(std::vector<std::string> shape, olc::Pixel c) : w(4), h(4), shape(shape), color(c) 
+    {};
+    void init(std::vector<std::string> n_shape, olc::Pixel c)
+    {
+        shape = n_shape;
+        color = c;
+    }
+    // rotar la ficha 90 grados a la izq o a la derecha
+    void rotate(Dir dir)
+    {
+        switch (dir)
+        {
+        case Dir::RIGHT:
+            // traspuesta
+            for (int i = 0; i < w; i += 1) {
+                for (int j = i+1; j < h; j += 1) {
+                    swap(i,j,j,i);
+                }
+            }
+            // reversa horizontal (columnas)
+            for(int fila = 0; fila < h; fila++)
+                std::reverse(shape[fila].begin(), shape[fila].end());
+            break;
+        case Dir::LEFT:
+            // traspuesta
+            for (int i = 0; i < w; i += 1) {
+                for (int j = i+1; j < h; j += 1) {
+                    swap(i,j,j,i);
+                }
+            }
+            // reversa vertical (filas)
+            std::reverse(shape.begin(), shape.end());
+            break;
+        default:
+            break;
+        }
+    }
+    // verificar si la ficha encaja en la posicion dada
+    bool doesFit(){};
+private:
+    void swap(int sx, int sy, int dx, int dy)
+    {
+        char aux = shape[sy][sx];
+        shape[sy][sx] = shape[dy][dx];
+        shape[dy][dx] = aux;
+    };
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,17 @@
 class TetrisGame : public olc::PixelGameEngine
 {
 private:
+    bool pixel = false;
+    Tile f;
+
     void DrawTile(int x, int y, Tile tile)
     {
-        // para cada fila
-        for(int j = 0; j < tile.h; j++)
+        for(int fila = 0; fila < tile.h; fila++)
         {
-            for(int i = 0; i < tile.w; i++)
+            for(int col = 0; col < tile.w; col++)
             {
-            // para cada fila
-                uint8_t fila = tile.shape[j];
-                // fila = 0b1000
-                // p1 = 0b1000 >> 3 = 1  si
-                // m  = 0b0001
-                // p2 = 0b1000 >> 2 = 1  si
-                if((fila >> (tile.w - i - 1)) & 0b0001)
-                {
-                    Draw(x + i, y + j);
-                }
+                if(tile.shape[fila][col] == 'x')
+                    Draw(col + x, fila + y, tile.color);
             }
         }
     }
@@ -35,25 +29,37 @@ public:
     bool OnUserCreate() override
     {
         //        3210  
-        Tile l({0b1000, 
-                0b1000, 
-                0b1000, 
-                0b1000});
-        Tile z({0b0000, 
-                0b0000, 
-                0b1100, 
-                0b0110});
-                
+        f.init({"0000",
+                "0x00",
+                "0x00",
+                "0xx0"}, olc::YELLOW);   
+
+        Tile g({"0000",
+                "0x00",
+                "0x00",
+                "0xx0"}, olc::BLUE);  
         // limpiar la pantalla de color gris oscuro
         Clear(olc::Pixel(0,0,0));
         // dibujar 4 esquinas
-        DrawTile(0,0,l);
-        DrawTile(3,0,z);
-        
+        DrawTile(0,0,f);
+
+        DrawTile(4,0,g);
+
         return true;
     }
+    // game loop
     bool OnUserUpdate(float fElapsedTime) override
     {
+
+        // temporizacion
+        // actualizar el estado
+        // -- atender eventos (teclado, mouse, otros)
+        if(GetKey(olc::Key::UP).bPressed)
+            f.rotate(Dir::RIGHT);
+
+        // dibujar el estado
+        // Clear(olc::BLACK);
+        DrawTile(4, 9, f);
         return true;
     }
 };
